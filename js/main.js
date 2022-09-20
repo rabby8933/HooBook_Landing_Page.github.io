b81a56e69810195e2ade6f7fbba4183fb3fdf32e
Fazle Rabby Khan <84661560+rabby8933@users.noreply.github.com>
2022-09-21 03:31:44 +0600

    sorry my boy

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Owl Carousel
 $(".owl-carousel").owlCarousel({
   loop: true,
+  center: true,
   margin: 10,
   nav: false,
   autoplay: true,
@@ -11,7 +12,7 @@ $(".owl-carousel").owlCarousel({
       items: 1,
     },
     600: {
-      items: 2,
+      items: 1,
     },
     1000: {
       items: 3,
